a20679083664e2be9b009a06d89db49b2249b814
gabrieleal <63738031+gabrieleal@users.noreply.github.com>
2021-03-25 02:38:54 -0300

    Add files via upload

--- controller/send.php
+++ controller/send.php
@@ -4,11 +4,18 @@
         include "_conexion.php";
 
         $resp=$_POST["obj"];
-        $resp=json_decode($resp,true);
-
-        //subject es el Asunto del email
-        $object = mail($resp["email"],$resp["asunto"],$resp["message"]);
+       
+        $headers="MIME-Version: 1-0 \r\n";
+        //especificamos la codificacion de caracteres
+        $headers.="Content-type: text/html; charset=iso-8859 \r\n";
+        //especificamos el remitente del correo
+        $headers.="From:Prueba Gaby <daniel.leal.78@enginner.com>";
         
+        //subject es el Asunto del email
+        $object = mail($resp["email"],$resp["asunto"],$resp["message"],$headers);
+        $headers="MIME-Version: 1-0 \r\n";
+        $headers.="Content-type: text/html; charset=iso-8859\r\n";
+        $headers.="From:Prueba Gaby <daniel.leal.78@enginner.com>";
         echo $object;
 
     }catch(Exception $e){
