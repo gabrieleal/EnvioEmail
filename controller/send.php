ec4a3b63fc911a238d56ad2ea90edaae26acf744
gabrieleal <63738031+gabrieleal@users.noreply.github.com>
2021-03-25 03:27:21 -0300

    Add files via upload

--- controller/send.php
+++ controller/send.php
@@ -9,13 +9,11 @@
         //especificamos la codificacion de caracteres
         $headers.="Content-type: text/html; charset=iso-8859 \r\n";
         //especificamos el remitente del correo
-        $headers.="From:Prueba Gaby <daniel.leal.78@enginner.com>";
+        $headers.="From:Prueba example <perez.godoy.78@enginner.com>";
         
         //subject es el Asunto del email
         $object = mail($resp["email"],$resp["asunto"],$resp["message"],$headers);
-        $headers="MIME-Version: 1-0 \r\n";
-        $headers.="Content-type: text/html; charset=iso-8859\r\n";
-        $headers.="From:Prueba Gaby <daniel.leal.78@enginner.com>";
+       
         echo $object;
 
     }catch(Exception $e){
